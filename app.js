976d317e0f528fb022c7bd68540fae5e21e37141
marselgray <marselgray@gmail.com>
2020-02-19 15:43:04 -0500

    Lesson: D3 Fundamentals manipulating selections

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
-var el = d3.select('body').append('p');
+var el = d3.select('body')
+	.append('p')
+	/* these are consider 
+	transformation methods */
+	.attr('class', 'foo')
+	.text('Hello world')
+	.classed('foobar', true)
+	.style('color', 'blue');
 
 console.log(el);
