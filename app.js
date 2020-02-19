68b48a8cfc9a22f261b13b2bc21b406b878518f9
marselgray <marselgray@gmail.com>
2020-02-19 16:37:41 -0500

    Lesson: D3 Fundamentals binding data

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
+var dataset = [10, 20, 30, 40, 50];
+
 var el = d3.select('body')
+	.selectAll('p')
+	.data(dataset)
+	.enter()
 	.append('p')
+	.text('hello world');
+
+	// .append('p')
 	/* these are consider 
 	transformation methods */
-	.attr('class', 'foo')
-	.text('Hello world')
-	.classed('foobar', true)
-	.style('color', 'blue');
+	// .attr('class', 'foo')
+	// .text('Hello world')
+	// .classed('foobar', true)
+	// .style('color', 'blue');
 
 console.log(el);
